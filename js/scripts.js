ec8e82c08767bb197c9363cfd5e88e54e423916e
Izuagbe Samuel Enakhe <izuagbesamuel73@gmail.com>
2024-05-28 23:27:56 +0100

    Add the sidebar and about page showcase section

--- js/scripts.js
+++ js/scripts.js
@@ -25,4 +25,12 @@ $(document).ready(function () {
             a = 1;
         }
     });
+
+    document.getElementById('open').addEventListener('click', () => {
+        document.getElementById("mySidenav").style.width = "100%";
+    })
+
+    document.getElementById('close').addEventListener('click', () => {
+        document.getElementById("mySidenav").style.width = "0";
+    })
 })
